747bcde8c0bc3efc56d63ae0e8e3a8ab51694022
Gareth Whittaker <gareth.whittaker@tombola.com>
2017-02-24 22:53:21 +0000

    add unit tests

---
+++ src/index.spec.js
@@ -0,0 +1,48 @@
+import test from 'ava'
+import sinon from 'sinon'
+import { Observable } from 'rxjs/Observable'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+import { animationLoop } from '../dist'
+
+let nextSpy
+let gameLoop
+
+test.beforeEach(t => {
+    nextSpy = sinon.spy(BehaviorSubject.prototype, 'next')
+
+    gameLoop = animationLoop()
+})
+
+test.afterEach(t => {
+    nextSpy.restore()
+})
+
+test('start', t => {
+    gameLoop.start()
+
+    t.true(nextSpy.calledOnce)
+    t.true(nextSpy.calledWithExactly(true))
+})
+
+test('stop', t => {
+    gameLoop.stop()
+
+    t.true(nextSpy.calledOnce)
+    t.true(nextSpy.calledWithExactly(false))
+})
+
+test('subscribe', t => {
+    const observerStub = sinon.stub()
+    const subscriberStub = sinon.stub()
+
+    const subscribeStub = sinon.stub(Observable.prototype, 'subscribe').returns(subscriberStub)
+
+    const gameLoopSubscriber = gameLoop.subscribe(observerStub)
+
+    t.true(subscribeStub.calledOnce)
+    t.true(subscribeStub.calledWithExactly(observerStub))
+
+    t.is(gameLoopSubscriber, subscriberStub)
+
+    subscribeStub.restore()
+})
